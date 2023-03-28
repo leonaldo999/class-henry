4ccca6b0a82432abec3d1b6715106fb3e11902b6
leonaldo999 <112677643+leonaldo999@users.noreply.github.com>
2023-03-28 01:14:23 -0500

    Bucles en JS .

---
+++ class/bucles.js
@@ -0,0 +1,18 @@
+//var suma = 0;
+//suma = suma +1;
+//suma = suma +2;
+//suma = suma +3;
+//suma = suma +4;
+//console.log(suma);
+
+//for
+for (var i=0 ; i<5 ; i++){
+    suma = suma + i;
+    console.log('Variable de iterancion: ', i);
+}
+console.log('Variable Suma: ', suma)
+//while
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
