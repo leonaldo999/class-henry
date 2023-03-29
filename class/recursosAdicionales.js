9df80adf002aa3fa3e23c19aad3ed17975eda0e7
leonaldo999 <112677643+leonaldo999@users.noreply.github.com>
2023-03-29 16:06:29 -0500

    Recursos Adicionales .

---
+++ class/recursosAdicionales.js
@@ -0,0 +1,31 @@
+//switch
+var expesion = case;
+switch (expresión) {
+  case valor1:
+    //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor1
+    [break;]
+  case valor2:
+    //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
+    [break;]
+  ...
+  case valorN:
+    //Declaraciones ejecutadas cuando el resultado de expresión coincide con valorN
+    [break;]
+  default:
+    //Declaraciones ejecutadas cuando ninguno de los valores coincide con el valor de la expresión
+    [break;]
+}
+
+//do while
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+// Expected output: "12345"
+
+//
