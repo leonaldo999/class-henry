399f460d8e8c6b8bebd16881e8fa516df882326f
leonaldo999 <112677643+leonaldo999@users.noreply.github.com>
2023-03-31 19:52:48 -0500

    Arrays o Arreglos.

---
+++ class/arrays.js
@@ -0,0 +1,16 @@
+var listaDeConpras = [];
+//Lista de compras:
+listaDeConpras[3] = 'Tomates';
+listaDeConpras[1] = 'Lechuga';
+
+//Lenght:
+console.log(listaDeConpras.length);
+// listaDeConpras[1];
+
+//Ver lista de compras:
+// var elementoDelArray = listaDeConpras[1];
+// console.log(elementoDelArray);
+
+//Lenght:
+// var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
+// console.log(nombres);
